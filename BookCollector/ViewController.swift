981758902891913d2eacb8960b776e3ec797cda0
Justin Deal <justindeal86@gmail.com>
2017-05-10 03:04:59 -0400

    Initial Commit

---
+++ BookCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BookCollector
+//
+//  Created by Justin Deal on 5/10/17.
+//  Copyright © 2017 J Dub Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
