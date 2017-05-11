8a1bac3f04b484e879660ab6bf3f8f89b57671bd
Justin Deal <justindeal86@gmail.com>
2017-05-11 16:17:45 -0400

    Added image picker to “Photos”

--- BookCollector/BookViewController.swift
+++ BookCollector/BookViewController.swift
@@ -8,17 +8,27 @@
 
 import UIKit
 
-class BookViewController: UIViewController {
+class BookViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var bookImageView: UIImageView!
 
     @IBOutlet weak var titleTextField: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
     }
 
     @IBAction func photosTapped(_ sender: Any) {
+        
+        //Tell system to get pictures from album
+        imagePicker.sourceType = .photoLibrary
+        
+        //Pull up another ViewController with the pictures
+        present(imagePicker, animated: true, completion: nil)
+        
     }
 
   
